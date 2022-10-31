72a4ed2d8d88d31a4d5f43c358a51de48c5ac0a6
Stepan Simsa <stepan.simsa@rossum.ai>
2022-10-31 14:52:58 +0100

    PR comments

--- docile/dataset/paths.py
+++ docile/dataset/paths.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from docile.dataset.types import OptionalImageSize
+
 
 class DataPaths:
     def __init__(self, dataset_path: Path):
@@ -17,13 +19,23 @@ class DataPaths:
     def annotation_path(self, docid: str) -> Path:
         return self.dataset_path / "annotations" / f"{docid}.json"
 
-    def cache_images_path(self, docid: str, size_tag: str) -> Path:
+    def cache_images_path(self, docid: str, size: OptionalImageSize) -> Path:
         """Path to directory with cached images for the individual pages."""
         directory_name = docid
+        size_tag = self._size_tag(size)
         if size_tag != "":
-            directory_name += f"__{size_tag}"
+            directory_name += f"__{self._size_tag(size)}"
         return self.dataset_path / "cached_images" / directory_name
 
     @staticmethod
     def cache_page_image_path(cache_images_path: Path, page_i: int) -> Path:
         return cache_images_path / f"{page_i}.png"
+
+    @staticmethod
+    def _size_tag(size: OptionalImageSize) -> str:
+        """Convert size param to string. This string is used as part of the cache path for images."""
+        if size == (None, None):
+            return ""
+        if isinstance(size, int):
+            return str(size)
+        return f"{size[0]}x{size[1]}"
---
+++ docile/dataset/types.py
@@ -0,0 +1,3 @@
+from typing import Optional, Tuple, Union
+
+OptionalImageSize = Union[int, Tuple[Optional[int], Optional[int]]]
