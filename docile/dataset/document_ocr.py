14397b06ee4f16958f71c5c930261893db8aa356
Matyáš Skalický <matyas.skalicky@rossum.ai>
2022-10-31 20:04:30 +0100

    kickoff basic pcc computations and simple metric

--- docile/dataset/document_ocr.py
+++ docile/dataset/document_ocr.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from typing import Any, Callable, Dict, Generator, List
 
 from docile.dataset.cached_object import CachedObject
+from docile.dataset.field import PCC, Field
 
 logger = logging.getLogger(__name__)
 
@@ -42,12 +43,17 @@ class DocumentOCR(CachedObject[Dict]):
         ocr_pred = self.get_model()(pdf_doc)
         return ocr_pred.export()
 
-    def get_all_words(self, page: int) -> List[Dict]:
+    def get_all_words(self, page: int) -> List[Field]:
         def yield_next_word(_page: int) -> Generator:
             ocr_page = self.content["pages"][_page]
             for block in ocr_page["blocks"]:
                 for line in block["lines"]:
                     for word in line["words"]:
-                        yield word
+                        yield Field.from_ocr(word)
 
         return list(yield_next_word(page))
+
+    def get_all_pccs(self, page: int) -> List[PCC]:
+        """Get all Pseudo Character Boxes (PCCs) on given page."""
+        words = self.get_all_words(page)
+        return [pcc for word in words for pcc in word.pccs]
